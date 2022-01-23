9e376ce97c31981d87f6a5350306eee592e29cc9
Wong <hwongkf8@gmail.com>
2022-01-23 17:19:22 -0500

    Added put and delete methods.

--- server/routes/campuses.js
+++ server/routes/campuses.js
@@ -1,5 +1,6 @@
 const router = require('express').Router()
 const Campus = require('../database/models/campuses')
+const Student = require('../database/models/students')
 const database = require('../database/database')
 
 router.get('/', async (req, res) => {
@@ -28,5 +29,46 @@ router.post('/', async(req, res) => {
         res.send(error.message)
     }
 })
+
+router.put('/', async(req, res) => {
+    try {
+        const updateCampus = await Campus.update(req.body, { 
+            where : { id : req.body.id },
+            returning : true 
+        });
+        res.status(200).json({
+            newData: updateCampus[1][0].dataValues
+        })
+    } catch (error) {
+        res.send(error.message)
+    }
+})
+
+router.delete('/:id', async(req, res) => {
+    try {
+        const inputid = req.params.id;
+        Campus.destroy({ where : { id : inputid } });
+        Student.update({ campusid : null }, { where : { campusid : inputid }
+        });
+        res.status(200).json({
+            outcome: `Deleted Campus with ${inputid}.`
+        })
+    } catch (error) {
+        res.send(error.message)
+    }
+})
+
+
+router.get('/:id/students', async(req, res) => {
+    try {
+        const inputid = req.params.id;
+        const students = await Student.findAll({where : { campusId : inputid }});
+        res.status(200).json(students);
+    } catch (error) {
+        res.send(error.message)
+    }
+})
+
+
 
 module.exports = router
--- server/routes/students.js
+++ server/routes/students.js
@@ -29,5 +29,31 @@ router.post('/', async(req, res) => {
     }
 })
 
+router.put('/', async(req, res) => {
+    try {
+        const updateStudent = await Student.update(req.body, { 
+            where : { id : req.body.id },
+            returning : true 
+        });
+        res.status(200).json({
+            newData: updateStudent[1][0].dataValues
+        })
+    } catch (error) {
+        res.send(error.message)
+    }
+})
+
+router.delete('/:id', async(req, res) => {
+    try {
+        const inputid = req.params.id;
+        await Student.destroy({ where : { id : inputid } });
+        res.status(200).json({
+            outcome: `Deleted Sudent with id ${inputid}.`
+        })
+    } catch (error) {
+        res.send(error.message)
+    }
+})
+
 
 module.exports = router
